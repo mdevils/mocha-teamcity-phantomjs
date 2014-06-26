01bb833438454a485b26af49023c945a01a1e80e
Marat Dulin <mdevils@yandex.ru>
2014-06-26 23:35:34 +0400

    Initial release

---
+++ mocha-teamcity-phantomjs.js
@@ -0,0 +1,61 @@
+(function() {
+
+    function Teamcity(runner) {
+        Mocha.reporters.Base.call(this, runner);
+        var stats = this.stats;
+
+        runner.on('suite', function (suite) {
+            if (suite.root) return;
+            console.log("##teamcity[testSuiteStarted name='" + escape(suite.title) + "']");
+        });
+
+        runner.on('test', function (test) {
+            console.log("##teamcity[testStarted name='" + escape(test.title) + "' captureStandardOutput='true']");
+        });
+
+        runner.on('fail', function (test, err) {
+            console.log("##teamcity[testFailed name='" + escape(test.title) + "' message='" + escape(err.message) + "' captureStandardOutput='true']");
+        });
+
+        runner.on('pending', function (test) {
+            console.log("##teamcity[testIgnored name='" + escape(test.title) + "' message='pending']");
+        });
+
+        runner.on('test end', function (test) {
+            console.log("##teamcity[testFinished name='" + escape(test.title) + "' duration='" + test.duration + "']");
+        });
+
+        runner.on('suite end', function (suite) {
+            if (suite.root) return;
+            console.log("##teamcity[testSuiteFinished name='" + escape(suite.title) + "' duration='" + (new Date() - suite.startDate) + "']");
+        });
+
+        runner.on('end', function () {
+            console.log("##teamcity[testSuiteFinished name='mocha.suite' duration='" + stats.duration + "']");
+        });
+    }
+
+    function escape(str) {
+        if (!str) return '';
+        return str
+            .replace(/\|/g, "||")
+            .replace(/\n/g, "|n")
+            .replace(/\r/g, "|r")
+            .replace(/\[/g, "|[")
+            .replace(/\]/g, "|]")
+            .replace(/\u0085/g, "|x")
+            .replace(/\u2028/g, "|l")
+            .replace(/\u2029/g, "|p")
+            .replace(/'/g, "|'");
+    }
+
+    var originalReporterMethod = mocha.reporter;
+    mocha.reporter = function (reporter) {
+        if (reporter === 'teamcity') {
+            return originalReporterMethod.call(this, Teamcity);
+        } else {
+            return originalReporterMethod.call(this, reporter);
+        }
+    };
+
+})();
